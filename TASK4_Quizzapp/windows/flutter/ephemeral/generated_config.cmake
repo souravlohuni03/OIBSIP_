113acb46066e461964f24a243844e279d6c24cc5
souravlohuni03 <89159841+souravlohuni03@users.noreply.github.com>
2023-04-19 00:32:28 +0530

    Add files via upload

---
+++ TASK4_Quizzapp/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\soura\\Downloads\\flutter_windows_3.3.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\soura\\Desktop\\flutter\\Quiz\\quiz_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\soura\\Downloads\\flutter_windows_3.3.1-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\soura\\Desktop\\flutter\\Quiz\\quiz_app"
+  "FLUTTER_ROOT=C:\\Users\\soura\\Downloads\\flutter_windows_3.3.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\soura\\Desktop\\flutter\\Quiz\\quiz_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\soura\\Desktop\\flutter\\Quiz\\quiz_app"
+  "FLUTTER_TARGET=C:\\Users\\soura\\Desktop\\flutter\\Quiz\\quiz_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\soura\\Desktop\\flutter\\Quiz\\quiz_app\\.dart_tool\\package_config.json"
+)
